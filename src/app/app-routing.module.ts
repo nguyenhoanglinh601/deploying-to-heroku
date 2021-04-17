9c55390b85fd87f0fedfd585c54179797584ffc5
nguyenhoanglinh601 <nguyenhoanglinh308@gmail.com>
2021-04-17 20:02:49 +0700

    add test routing component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TestRoutingComponent } from './test-routing/test-routing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"test", component: TestRoutingComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestRoutingComponent } from './test-routing/test-routing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestRoutingComponent
   ],
   imports: [
     BrowserModule,
